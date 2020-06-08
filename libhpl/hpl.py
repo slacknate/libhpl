1c3d215dbceee0aad9d00ebc82c11a2279eb8bae
Nate Duarte <slacknate@gmail.com>
2020-06-07 21:38:13 -0400

    [hpl] clean up code
     - add mad comments
     - use not-single-letter var names
     - simplify some algorithms
     - in general try to make it more obvious what is going on

--- libhpl/hpl.py
+++ libhpl/hpl.py
@@ -1,85 +1,100 @@
-import os
-import struct
-
 from PIL import Image, ImageDraw
 
-COLOR_SIZE = 20
-SQUARE_SIZE = 16
-IMG_SIDE_LEN = COLOR_SIZE * SQUARE_SIZE
+HPL_CHUNK_SIZE = 3
+HPL_MAX_COLORS = 256
+PALETTE_SQUARE_SIZE = 16
 
-HPAL_HDR = (b"HPAL%\x01\x00\x00 \x04\x00\x00\x00\x01\x00\x00\x00\x00"
-            b"\x00\x00\x00\x00\x00\x00\x01\x00\x00\x10\x00\x00\x00\x00")
+HPAL_HEADER = (b"HPAL%\x01\x00\x00 \x04\x00\x00\x00\x01\x00\x00\x00\x00"
+               b"\x00\x00\x00\x00\x00\x00\x01\x00\x00\x10\x00\x00\x00\x00")
 
 
-def convert_to_hpal(f):
+def convert_to_hpal(image_path):
     """
+    Convert a PNG image to an HPL palette file.
+
+    Note that PNG images are not required to have a palette and
+    PIL will complain about images that do not contain one (i.e. raise an exception).
+
     Original script by resnovae, slight tweak by Labryz.
+    Cleaned up some by slacknate.
     """
-    with Image.open(f) as i:
-        p = i.getdata().getpalette()
+    out = image_path.replace(".png", ".hpl")
 
-    out = os.path.splitext(f)[0]+".hpl"
-    with open(out, "wb") as f:
-        f.write(HPAL_HDR)
-        a = 0
-        for c in [p[i:i+1] for i in range(0, len(p), 1)][::-1]:
-            f.write(c)
-            a += 1
-            if a == 3:
-                f.write(b"\xff")
-                a = 0
+    with Image.open(image_path) as image:
+        palette = image.getdata().getpalette()
 
+    with open(out, "wb") as hpl_fp:
+        hpl_fp.write(HPAL_HEADER)
 
-def _blap(color_data):
-    a = 0
-    for c in color_data:
-        if a == 3:
-            a = 0
-            continue
+        # Iterate our palette data in reverse.
+        remaining = palette[::-1]
+        while remaining:
+            # We grab 3 byte chunks as each of these is an RGB tuple.
+            chunk = remaining[0:HPL_CHUNK_SIZE]
+            remaining = remaining[HPL_CHUNK_SIZE:]
 
-        yield c
-        a += 1
+            # When we write a chunk we append 0xFF. This seems to be some kind of marker.
+            # It could also be other information we do not need? Perhaps an alpha channel?
+            hpl_fp.write(chunk + b"\xFF")
 
 
-def convert_from_hpal(f):
-    with open(f, "rb") as i:
-        data = i.read()
-        _, color_data = data.split(HPAL_HDR)
+def _remove_xff(color_data):
+    """
+    Remove every fourth byte of the color data we read
+    from the palette file. Looking at `convert_to_hpal` we can
+    see that 0xFF is inserted after every third byte of our palette data.
+    """
+    remaining = color_data
+    while remaining:
+        chunk = remaining[0:HPL_CHUNK_SIZE]
+        remaining = remaining[HPL_CHUNK_SIZE + 1:]
 
-    palette = b"".join(list(_blap(color_data)))
+        yield chunk
 
-    a = 0
-    curr = []
-    color_list = []
-    for c in palette:
-        if a == 3:
-            a = 0
-            color_list.append(tuple(curr[::-1]))
-            curr = []
-            continue
 
-        curr.append(struct.unpack("B", c)[0])
-        a += 1
+def convert_from_hpal(palette_path, color_size):
+    img_side_len = color_size * PALETTE_SQUARE_SIZE
 
-    num_colors = len(color_list)
-    color_list = ([(0, 0, 0)] * (256 - num_colors)) + color_list
+    with open(palette_path, "rb") as hpl_fp:
+        data = hpl_fp.read()
+        _, color_data = data.split(HPAL_HEADER)
 
-    out = os.path.splitext(f)[0]+".png"
-    with Image.new("P", (IMG_SIDE_LEN, IMG_SIDE_LEN)) as i:
-        i.putpalette(palette[::-1])
-        d = ImageDraw.Draw(i)
+    # Create our raw palette data.
+    palette = b"".join(_remove_xff(color_data))
 
+    out = palette_path.replace(".hpl", ".png")
+
+    # We create a "P" image as that is a palette image. This will create a PNG
+    # with a palette, meaning we can pass this image to `convert_to_hpal` and it will work.
+    with Image.new("P", (img_side_len, img_side_len)) as image_fp:
+        image_fp.putpalette(palette[::-1])
+        d = ImageDraw.Draw(image_fp)
+
+        # Our palette will have 256 colors max. We draw this in image form as a 16x16 square of "pixels".
+        # The pixel size is determined by `color_size`.
         x = 0
         y = 0
-        curr = 0
-        for _ in color_list:
-            i.palette.colors[(x, y)] = curr
-            dim = [((x * COLOR_SIZE), (y * COLOR_SIZE)), ((x * COLOR_SIZE) + COLOR_SIZE, (y * COLOR_SIZE) + COLOR_SIZE)]
-            d.rectangle(dim, fill=(x, y))
-            x += 1
-            if x == SQUARE_SIZE:
-                x = 0
-                y += 1
-            curr += 1
 
-        i.save(out)
+        # Iterate over all 256 colors of the palette.
+        for palette_index in range(HPL_MAX_COLORS):
+            # Honestly? This might be jank. But `image_fp.putpalette` creates a raw mode palette.
+            # When we go to draw the rectangle in a few lines, calling out a fill looks into this colors
+            # dict that is set on the palette and it ignores the raw data we have set in it, and actually
+            # will overwrite that data for some reason.
+            # PIL wants the index of this dict to be a tuple so we just make it our XY index of this color square.
+            # The value is our palette index and NOT THE ACTUAL RGB VALUE, as the RGB value will be grabbed
+            # from the palette (I believe it's a mapping sort of deal).
+            image_fp.palette.colors[(x, y)] = palette_index
+
+            # Draw a square that is a single color from the palette.
+            dim = [((x * color_size), (y * color_size)), ((x * color_size) + color_size, (y * color_size) + color_size)]
+            d.rectangle(dim, fill=(x, y))
+
+            # Move to the next column in the row. Once we reach the end of the row we head back to column 0.
+            x += 1
+            x %= PALETTE_SQUARE_SIZE
+            # Once we have filled a row of 16 we move to the next row.
+            if x == 0:
+                y += 1
+
+        image_fp.save(out)
