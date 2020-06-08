95b120baf8568340a72d01968925ea2cf645f8c9
Nate Duarte <slacknate@gmail.com>
2020-06-07 21:03:47 -0400

    [__main__] add console program for basic lib usage

---
+++ libhpl/__main__.py
@@ -0,0 +1,40 @@
+import os
+import argparse
+
+from .hpl import convert_to_hpal, convert_from_hpal
+
+DEF_COLOR_SQUARE_SIZE = 20
+
+
+def abs_path(value):
+    value = os.path.abspath(value)
+
+    if not os.path.exists(value):
+        raise argparse.ArgumentError("Invalid file path! Does not exist!")
+
+    return value
+
+
+def main():
+    parser = argparse.ArgumentParser("hpl")
+    subparsers = parser.add_subparsers(title="commands")
+
+    frompng = subparsers.add_parser("frompng")
+    frompng.add_argument(dest="image", type=abs_path, help="Image input path.")
+
+    topng = subparsers.add_parser("topng")
+    topng.add_argument(dest="palette", type=abs_path, help="Palette input path.")
+    topng.add_argument("--size", "-s", dest="size", type=int,
+                       default=DEF_COLOR_SQUARE_SIZE, help="Size of color blocks in pixels.")
+
+    args, _ = parser.parse_known_args()
+
+    if getattr(args, "image", None) is not None:
+        convert_to_hpal(args.image)
+
+    elif getattr(args, "palette", None) is not None:
+        convert_from_hpal(args.palette, args.size)
+
+
+if __name__ == "__main__":
+    main()
